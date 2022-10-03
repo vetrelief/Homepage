97b74cbfb7716e270517163e7ff673f1a03375fb
barrowpaul <barrowpaul738@gmail.com>
2022-10-03 13:54:50 +0300

    Fixed testimonials auto slide, video autoplay on scroll and re-aranged what we treat icons

--- assets/js/video-play.js
+++ assets/js/video-play.js
@@ -1,22 +1,15 @@
-   
-    window.addEventListener('scroll', videoScroll);
+window.addEventListener('scroll', videoScroll);
+function videoScroll() {
 
-    function videoScroll() {
-
-        var windowHeight = window.innerHeight,
-        videoEl = document.querySelectorAll('#petVideoAutoPlay[autoplay]');
-        for (var i = 0; i < videoEl.length; i++) {
-
-        var thisVideoEl = videoEl[i],
-            
-            videoHeight = thisVideoEl.clientHeight,
-            videoClientRect = thisVideoEl.getBoundingClientRect().top;
-        if ( videoClientRect <= ( (windowHeight) - (videoHeight*.5) ) && videoClientRect >= ( 0 - ( videoHeight*.5 ) ) ) {
-            thisVideoEl.play();
-        } else {
-            thisVideoEl.pause();
-        }
-
-        }
-
+    var windowHeight = window.innerHeight,
+    videoEl = document.querySelectorAll('#petVideoAutoPlay[autoplay]');
+    var thisVideoEl = videoEl[0],
+    videoHeight = thisVideoEl.clientHeight,
+    videoClientRect = thisVideoEl.getBoundingClientRect().top;
+    if ( videoClientRect <= ( (windowHeight) - (videoHeight*.5) ) && videoClientRect >= ( 0 - ( videoHeight*.5 ) ) ) {
+        thisVideoEl.play();
+    } else {
+        thisVideoEl.pause();
     }
+
+}
